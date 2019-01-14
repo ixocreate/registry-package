a795b8e47745d46510b48219c454792e204e1708
Andreas Friedrich <30212702+Friedrich-A@users.noreply.github.com>
2019-01-14 11:27:18 +0100

    added Factory

---
+++ src/Factory/RegistryFactory.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @link https://github.com/ixocreate
+ * @copyright IXOCREATE GmbH
+ * @license MIT License
+ */
+
+declare(strict_types=1);
+
+namespace Ixocreate\Registry\Factory;
+
+use Ixocreate\Contract\ServiceManager\FactoryInterface;
+use Ixocreate\Contract\ServiceManager\ServiceManagerInterface;
+use Ixocreate\Database\Repository\Factory\RepositorySubManager;
+use Ixocreate\Registry\Registry;
+use Ixocreate\Registry\RegistrySubManager;
+use Ixocreate\Registry\Repository\RegistryRepository;
+use Ixocreate\Schema\Builder;
+
+class RegistryFactory implements FactoryInterface
+{
+    /**
+     * @param ServiceManagerInterface $container
+     * @param $requestedName
+     * @param array|null $options
+     * @return mixed
+     */
+    public function __invoke(ServiceManagerInterface $container, $requestedName, array $options = null)
+    {
+        /** @var RepositorySubManager $test */
+        $repositorySubManager = $container->get(RepositorySubManager::class);
+        $registryRepository = $repositorySubManager->get(RegistryRepository::class);
+
+        /** @var RegistrySubManager $registrySubManager */
+        $registrySubManager = $container->get(RegistrySubManager::class);
+
+        /** @var Builder $builder */
+        $builder = $container->get(Builder::class);
+
+        return new Registry($registryRepository, $registrySubManager, $builder);
+    }
+}
