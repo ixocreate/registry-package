916f065484cb357affda02902b167fcc8e12896f
Andreas Friedrich <30212702+Friedrich-A@users.noreply.github.com>
2019-01-14 11:25:28 +0100

    added Repository

---
+++ src/Repository/RegistryRepository.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @link https://github.com/ixocreate
+ * @copyright IXOCREATE GmbH
+ * @license MIT License
+ */
+
+declare(strict_types=1);
+
+namespace Ixocreate\Registry\Repository;
+
+use Ixocreate\Database\Repository\AbstractRepository;
+use Ixocreate\Registry\Entity\Registry;
+
+class RegistryRepository extends AbstractRepository
+{
+
+    /**
+     * @return string
+     */
+    public function getEntityName(): string
+    {
+        return Registry::class;
+    }
+
+
+}
