5798dfa9ed20a9a67d7a3478e2b4121c9947751d
Andreas Friedrich <30212702+Friedrich-A@users.noreply.github.com>
2019-01-14 11:26:34 +0100

    added RegistrySubManager

---
+++ src/RegistrySubManager.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @link https://github.com/ixocreate
+ * @copyright IXOCREATE GmbH
+ * @license MIT License
+ */
+
+declare(strict_types=1);
+
+namespace Ixocreate\Registry;
+
+use Ixocreate\ServiceManager\SubManager\SubManager;
+
+class RegistrySubManager extends SubManager
+{
+}
