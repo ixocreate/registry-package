f2f4710bf5a2150fd1aafe3972b6238a90b75726
Andreas Friedrich <30212702+Friedrich-A@users.noreply.github.com>
2019-01-30 13:57:03 +0100

    switched from Registry to RegistryInterface

--- bootstrap/servicemanager.php
+++ bootstrap/servicemanager.php
@@ -9,11 +9,12 @@ declare(strict_types=1);
 
 namespace Ixocreate\Registry;
 
+use Ixocreate\Contract\Registry\RegistryInterface;
 use Ixocreate\Registry\Factory\RegistryFactory;
 use Ixocreate\ServiceManager\ServiceManagerConfigurator;
 
 /** @var ServiceManagerConfigurator $serviceManager */
 $serviceManager->addSubManager(RegistrySubManager::class);
 
-$serviceManager->addFactory(Registry::class, RegistryFactory::class);
+$serviceManager->addFactory(RegistryInterface::class, RegistryFactory::class);
 
