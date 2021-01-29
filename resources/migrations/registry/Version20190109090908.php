635d5c3daefa01c2bb2d7882e1a74b65eac033e5
Bernhard Efler <1978420+bernhard-efler@users.noreply.github.com>
2021-01-29 11:30:01 +0100

    update migrations

--- resources/migrations/registry/Version20190109090908.php
+++ resources/migrations/registry/Version20190109090908.php
@@ -10,7 +10,7 @@ declare(strict_types=1);
 namespace Ixocreate\Migration;
 
 use Doctrine\DBAL\Schema\Schema;
-use Doctrine\DBAL\Types\Type;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\Migrations\AbstractMigration;
 use Ixocreate\Schema\Type\DateTimeType;
 use Ixocreate\Schema\Type\SchemaType;
@@ -21,7 +21,7 @@ final class Version20190109090908 extends AbstractMigration
     {
         $table = $schema->createTable('registry_registry');
 
-        $table->addColumn('id', Type::STRING);
+        $table->addColumn('id', Types::STRING);
         $table->addColumn('value', SchemaType::serviceName());
         $table->addColumn('createdAt', DateTimeType::serviceName());
         $table->addColumn('updatedAt', DateTimeType::serviceName());
